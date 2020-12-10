c154527282c1755f80421e95ecc770c07cf77982
Bharti20 <bhartikumari20@navgurukul.org>
2020-12-11 00:07:36 +0800

    i have solve two logical questions

---
+++ add_multiple_integer_value_to_one.py
@@ -0,0 +1,5 @@
+List=[11, 33, 50]
+i=0
+while i<len(List):
+    print(List[i],end='')
+    i=i+1
---
+++ marge_2nd_list.py
@@ -0,0 +1,7 @@
+List1=[2,4,5,6]
+List2=[6,9,1,3]
+i=0
+while i<len(List2):
+    List1.append(List2[i])
+    i=i+1
+print(List1)
